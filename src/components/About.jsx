5b5809fd7a2918bf4b10591aa895c92eb7b9a56c
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2024-01-09 03:22:32 -0330

    mouse

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Tilt } from "react-tilt";
+import {motion} from "framer-motion";
 
+import {styles} from "../styles.js";
+import {serives} from "../styles.js"
+import {fadeIn, textVariant} from "../utils"
+
+import 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVarient()} >
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+    </>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import ComputersCanvas from "./canvas/Computers";
-// import ComputersCanvas from "./canvas";
 
 const Hero = () => {
   return (
@@ -25,8 +24,24 @@ const Hero = () => {
             applications.
           </p>
         </div>
+      </div>
 
-        <ComputersCanvas />
+      <ComputersCanvas />
+
+      <div className=" absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-xl border-4 border-secondary flex justify-center items-center p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
