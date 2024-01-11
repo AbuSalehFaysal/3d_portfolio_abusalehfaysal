74de81ed77886d8ca15225e2af90d0f1de1ce565
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2024-01-11 03:03:43 -0330

    section wrapper

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className=" absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="xs:bottom-10 bottom-32 absolute w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-xl border-4 border-secondary flex justify-center items-center p-2">
             <motion.dev
